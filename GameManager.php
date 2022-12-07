ebc5cc374a185216537e11631406440f941df9d8
anissaabenn <107192853+anissaabenn@users.noreply.github.com>
2022-12-07 14:50:38 +0100

    Creation du GameManager

---
+++ GameManager.php
@@ -0,0 +1,15 @@
+<?php
+
+class GameManager{
+    private $games;
+
+    public function addGame($game){
+        $this->games[] = $game;
+    }
+
+    public function getGames(){
+        return $this->games;
+    }
+}
+
+?>
