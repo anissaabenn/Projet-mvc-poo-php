bb05def588362478c4fb41d3a934223e165acc97
anissaabenn <107192853+anissaabenn@users.noreply.github.com>
2022-12-12 16:27:01 +0100

    Debut du ROOTING simple + affichage de vue

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-require_once "view/home.view.php";
+if(empty($_GET['page'])){
+    require_once "view/home.view.php";
+}else {
+    switch ($_GET['page']) {
+        case 'accueil':
+            require_once "view/home.view.php";
+            break;
+    }
+}
+
 
 ?>
