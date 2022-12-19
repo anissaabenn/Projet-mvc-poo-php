d683633cc14d4993b8548b982e45673cb908b996
anissaabenn <107192853+anissaabenn@users.noreply.github.com>
2022-12-19 11:59:05 +0100

    EDIT -step 4
    On récupère bien les datas

--- view/edit.games.view.php
+++ view/edit.games.view.php
@@ -6,3 +6,16 @@ $content = ob_get_clean();
 $title = "Editer :  " . $game->getTitle();
 require_once "view/base.view.php";
 ?>
+
+<form  method="POST" action="<?= URL ?>games/editvalid">
+<div class="form-group">
+    <label for="title">Titre du jeu</label>
+    <input type="text" class="form-control" value="<?= $game->getTitle() ?>" name="title" id="title">
+</div>
+<div class="form-group">
+    <label for="nbPlayers">Nombre de joueurs</label>
+    <input type="number" class="form-control" value="<?= $game->getnbPlayers() ?>" name="nbPlayers" id="nbPlayers">
+</div>
+<input type="hidden" name="id-game" value="<?= $game->getId() ?>">
+<button type="submit" class="btn btn-primary">Ajouter</button>
+</form>
